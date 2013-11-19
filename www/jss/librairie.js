f98ed0d8ca5135c582cbbc719b68b1dfdd7bbe53
ERIC <ERIC@DELLXPS8300>
2013-11-19 19:10:23 +0100

    Geolocalisation

--- www/jss/librairie.js
+++ www/jss/librairie.js
@@ -30,4 +30,47 @@ myStore.displayUnsupported = function()  {
     alert("Votre navigateur ne supporte pas le HTML5 et le stockage serveur. Impossible de mémoriser votre saisie.")
 }
 
+myGps = {}
+myGps.init = function()  {
+    myGps.update();
+    return this;
+}
+myGps.update = function()  {
+    navigator.geolocation.getCurrentPosition(myGps._set, myGps._onError);
+    return this;
+}
+myGps._set = function(position)  {
+    var infopos = "";
+    infopos += "Latitude : "+position.coords.latitude +"<br/>";
+    infopos += "Longitude: "+position.coords.longitude+"<br/>";
+    infopos += "Altitude : "+position.coords.altitude +"<br/>";
+    myStore.set("lastPosition",infopos);
+    return this;
+}
+myGps.get = function(key)  {
+    return myStore.get("lastPosition");
+}
+myGps.reset = function(key)  {
+    this.update()
+    return this;
+}
+myGps._onError = function(error)  {
+    var info = "<b>Erreur lors de la géolocalisation : ";
+    switch(error.code) {
+        case error.TIMEOUT:
+            info += "Timeout !<br/>";
+            break;
+        case error.PERMISSION_DENIED:
+            info += "Vous n’avez pas donné la permission<br/>";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            info += "La position n’a pu être déterminée<br/>";
+            break;
+        case error.UNKNOWN_ERROR:
+            info += "Erreur inconnue";
+            break;
+            info += "</b>";
+    }
+    myStore.set("lastPosition",info);
+}
 
